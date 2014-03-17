54d747056cb71f1a7ace625eb81d5d8fff39a644
Chris Sauve <chris@pxldot.com>
2014-03-16 21:20:20 -0400

    Complete refactor to allow manual resizing

--- jquery.squishy.js
+++ jquery.squishy.js
@@ -1,58 +1,74 @@
-(function( $ ){
-  
-  $.fn.squishy = function(options) {
+(function($) {
+
+$.fn.squishy = function(options) {
 
     // Setup options
     var settings = $.extend({
-      'minSize': -10000,
-      'maxSize': 10000,
-      'maxWidth': 10000,
-      'minWidth' : -10000},
-      options);
-  
-    return this.each(function() {
+        "minSize"         : -10000,
+        "maxSize"         : 10000,
+        "maxWidth"        : 10000,
+        "minWidth"        : -10000,
+        "runAutomatically": true
+    }, options);
 
-      // Store the object
-      var $this = $(this);
-      
-      // Does the resizing
-      var resizer = function () {
-    
-        // Add the wrapper span
-        var theText = $this.html();
-        $this.html("<span id='checkSizeForSquishing'>" + theText + "</span>");
+    var that = this;
 
-        // Figuring out the relevant widths
-        var spanWidth = $("#checkSizeForSquishing").width();
-        var blockWidth = Math.max(parseFloat(settings.minWidth), Math.min($this.width(), parseFloat(settings.maxWidth)));
-        var fontSize = parseFloat($this.css("font-size"));
+    // Does the resizing
+    var resizer = function(e, subsetSelector) {
 
-        // Set the target size (restricted by min/max sizes)
-        var targetSize = fontSize*blockWidth/spanWidth;
-        //alert(targetSize);
-        targetSize = Math.floor(Math.min(Math.max(targetSize, parseFloat(settings.minSize)), parseFloat(settings.maxSize)));
+        var actOn;
+        if(subsetSelector) {
+            actOn = that.filter(function() {
+                return $(this).is(subsetSelector);
+            });
+        } else {
+            actOn = that;
+        }
 
-        $this.css('white-space', 'nowrap');
-        $this.css('font-size', targetSize);
+        actOn.each(function() {
+            $this = $(this);
 
-        // Remove the wrapper span
-        $this.html(theText);
-        
-      };
+            // Add the wrapper span
+            var theText = $this.html(),
+                $span   = $this.html("<span id='checkSizeForSquishing'>" + theText + "</span>").children("#checkSizeForSquishing");
 
-      // Set the font-size to 1px so spanWidth would not be too large (over parent)
-      $this.css('font-size', '1px');
+            // Figuring out the relevant widths
+            var spanWidth = $span.width(),
+                blockWidth = Math.max(parseFloat(settings.minWidth), Math.min($this.width(), parseFloat(settings.maxWidth))),
+                fontSize = parseFloat($this.css("font-size"));
 
-      // Initial will get it bigger but not too big
-      resizer();
-      // Second call fits exactly
-      resizer();
-      
-      // Calls the resize on viewport width or orientation change
-      $(window).on('resize orientationchange', resizer);
-      
-    });
+            // console.log("fontSize: " + fontSize + ", blockWidth: " + blockWidth + ", spanWidth: " + spanWidth);
 
-  };
+            // Set the target size (restricted by min/max sizes)
+            var targetSize = fontSize*blockWidth/spanWidth;
+            targetSize = Math.floor(Math.min(Math.max(targetSize, parseFloat(settings.minSize)), parseFloat(settings.maxSize)));
 
-})( jQuery );
+            $this.css({"white-space": "nowrap", "font-size": targetSize, "text-align": "justify"}).html(theText);
+        });
+
+    };
+
+    if(settings.runAutomatically) {
+        // Initial will get it bigger but not too big
+        resizer();
+
+        // Calls the resize on viewport width or orientation change
+        $(window).on("resize orientationchange", resizer);
+    }
+
+    return {
+        resize: function(selector) {
+            return resizer(null, selector);
+        },
+
+        makeAutomatic: function() {
+            if(!settings["runAutomatically"]) {
+                settings["runAutomatically"] = true;
+                resizer();
+                $(window).on("resize orientationchange", resizer);
+            }
+        }
+    };
+};
+
+})(jQuery);
